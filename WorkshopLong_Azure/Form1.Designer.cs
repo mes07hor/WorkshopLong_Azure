71d24efb04e67edad441fa495f0502fe76ad4296
mes07hor <mes07hor@gmail.com>
2021-01-30 17:26:22 +0900

    Add project files.

---
+++ WorkshopLong_Azure/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace WorkshopLong_Azure
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(527, 486);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(41, 447);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 42);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "決定";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(41, 380);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged_1);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(41, 309);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(165, 23);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "氏名（例：阪大太郎）";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Meiryo UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(41, 286);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(155, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "名前を入力してください";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Meiryo UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(41, 357);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(173, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "グループを選択してください";
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 120000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Meiryo UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(218, 456);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 30);
+            this.label2.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Meiryo UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(41, 213);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(96, 35);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "待機中";
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(905, 554);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "WorkshopLog";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ WorkshopLong_Azure/Form1.cs
@@ -0,0 +1,333 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+
+
+namespace WorkshopLong_Azure
+{
+    public partial class Form1 : Form
+    {
+        private SpeechRecognizer recognizer;
+        TaskCompletionSource<int> stopRecognition;
+        public Form1()
+        {
+            InitializeComponent();
+            //var config = SpeechConfig.FromSubscription("5a9e0d42e58545c997c9d85fd51f413f", "japaneast");
+            var config = SpeechConfig.FromSubscription("93f21ea575fc42eba2dcf29285dd3190", "japaneast");
+
+            config.EnableDictation();
+            config.SpeechRecognitionLanguage = "ja-JP";
+            var audioConfig = AudioConfig.FromDefaultMicrophoneInput();
+            recognizer = new SpeechRecognizer(config,audioConfig);
+            stopRecognition = new TaskCompletionSource<int>();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+            //GetTableCatalog();
+            //comboBox1.DataSource = TableCatalog;
+            comboBox1.Items.Add("Adults1");
+            comboBox1.Items.Add("Adults2");
+            comboBox1.Items.Add("Students1");
+            comboBox1.Items.Add("Students2");
+            comboBox1.Items.Add("voicerecord");
+        }
+        private static double time = 0; //total speaking time
+
+
+        private static string name = null;    //participant name Initialize
+        public bool nameInput = false;
+        static bool speechbool = false;
+
+        
+        private async void startRecognition(object sender, EventArgs e)
+        {
+            name = textBox1.Text;
+
+            nameInput = true;
+
+
+            System.Diagnostics.Debug.WriteLine("Say something");
+
+
+            // Starts speech recognition, and returns after a single utterance is recognized. The end of a
+            // single utterance is determined by listening for silence at the end or until a maximum of 15
+            // seconds of audio is processed.  The task returns the recognition text as result. 
+            // Note: Since RecognizeOnceAsync() returns only a single utterance, it is suitable only for single
+            // shot recognition like command or query. 
+            // For long-running multi-utterance recognition, use StartContinuousRecognitionAsync() instead.
+            //var result = await recognizer.RecognizeOnceAsync();
+
+            recognizer.Recognizing += (s, e) =>
+            {
+                System.Diagnostics.Debug.WriteLine($"RECOGNIZING: Text={e.Result.Text}");
+                label5.Invoke(new Action(()=> { label5.Text = "認識中"; }));
+
+            };
+
+            recognizer.Recognized += (s, e) =>
+            {
+                if (e.Result.Reason == ResultReason.RecognizedSpeech)
+                {
+                    System.Diagnostics.Debug.WriteLine($"RECOGNIZED: Text={e.Result.Text}");
+                    //string text1 = e.Result.Text;
+                    //label2.Text = text1;
+                    label1.Invoke(new Action(() => { label1.Text = e.Result.Text; }));
+
+                    System.Diagnostics.Debug.WriteLine($"RECOGNIZED: Time={e.Result.Duration}");
+                    time = time + e.Result.Duration.TotalSeconds;
+                    System.Diagnostics.Debug.WriteLine($"RECOGNIZED: Total Time={time}");
+                    label5.Invoke(new Action(()=> { label5.Text = "待機中"; }));
+
+                    submitsentence(e.Result.Text);
+
+                }
+                else if (e.Result.Reason == ResultReason.NoMatch)
+                {
+                    System.Diagnostics.Debug.WriteLine($"NOMATCH: Speech could not be recognized.");
+                }
+                speechbool = true;
+            };
+
+
+
+            recognizer.Canceled += (s, e) =>
+            {
+                Console.WriteLine($"CANCELED: Reason={e.Reason}");
+
+                if (e.Reason == CancellationReason.Error)
+                {
+                    Console.WriteLine($"CANCELED: ErrorCode={e.ErrorCode}");
+                    Console.WriteLine($"CANCELED: ErrorDetails={e.ErrorDetails}");
+                    Console.WriteLine($"CANCELED: Did you update the subscription info?");
+                }
+
+                stopRecognition.TrySetResult(0);
+            };
+
+            recognizer.SessionStopped += (s, e) =>
+            {
+                Console.WriteLine("\n    Session stopped event.");
+                stopRecognition.TrySetResult(0);
+            };
+
+            await recognizer.StartContinuousRecognitionAsync();
+            await Task.WhenAny(stopRecognition.Task);
+        }
+        static bool btnClickFirstTime = true;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (btnClickFirstTime)
+            {
+                startRecognition(sender, e);
+                btnClickFirstTime = false;
+                label2.Text="ようこそ、"+textBox1.Text.ToString()+"さん";
+            }
+
+        }
+        
+        static void submitsentence(string sentence)
+        {
+            string constr = @"Data Source=sfws.c7t5mjpl9wt1.ap-northeast-1.rds.amazonaws.com,1433;
+                                Initial Catalog=Workshop;Connect Timeout=20;Persist Security Info=True;User ID=SSD;Password=lcopLCOP";
+
+
+
+            using (SqlConnection con = new SqlConnection(constr))
+            using (var command = con.CreateCommand())
+            {
+                try
+                {
+                    DateTime date = DateTime.Now;
+                    string Date = date.ToString("yyyy-MM-dd HH:mm:ss.fff");         
+
+                    string commandText = "INSERT INTO " + "SentenceTest2" + " (Date, sentence, username,selectedgroup) VALUES(@Date, @sentence, @username,@selectedgroup)";
+                    con.Open();
+                    //command.CommandText = @"INSERT INTO voicerecord (Date,time,username) VALUES (@Date,@time,@username)";
+                    command.CommandText = @commandText;
+
+                    command.Parameters.Add(new SqlParameter("@Date", Date));
+                    command.Parameters.Add(new SqlParameter("@sentence", sentence));
+                    command.Parameters.Add(new SqlParameter("@username", name));
+                    command.Parameters.Add(new SqlParameter("@selectedgroup", selectedTable));
+
+
+                    command.ExecuteNonQuery();
+                    Console.WriteLine("{0} success", Date);
+
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception.Message);
+                    throw;
+                }
+                finally
+                {
+                    con.Close();
+                }
+            }
+        }
+
+        static void submitdata(double time)
+        {
+            //string constr = @"Data Source=fe80::f8ad:a765:a2fa:5fcf%12,1433;Initial Catalog=Test;Connect Timeout=2;Persist Security Info=True;User ID=ssd;Password=lcop";
+            //121.87.109.100\SQLEXPRESS,1433
+
+            //string constr = @"Data Source=ssdlab.cruxuvlbhlee.us-east-2.rds.amazonaws.com,1433;
+            //                    Initial Catalog=Workshop;Connect Timeout=20;Persist Security Info=True;User ID=ssd;Password=lcoplcop";
+            
+            
+            
+            string constr = @"Data Source=sfws.c7t5mjpl9wt1.ap-northeast-1.rds.amazonaws.com,1433;
+                                Initial Catalog=Workshop;Connect Timeout=20;Persist Security Info=True;User ID=SSD;Password=lcopLCOP";
+
+
+
+            using (SqlConnection con = new SqlConnection(constr))
+            using (var command = con.CreateCommand())
+            {
+                try
+                {
+                    DateTime date = DateTime.Now;
+                    string Date = date.ToString("yyyy-MM-dd HH:mm:ss.fff");
+                    string commandText;
+
+                    switch (selectedTable)
+                    {
+                        case "Adults1":
+                        case "Adults2":
+                            commandText = "INSERT INTO " + "Adults" + " (Date, time, username,selectedgroup) VALUES(@Date, @time, @username,@selectedgroup)";
+                            break;
+                        case "Students1":
+                        case "Students2":
+                            commandText = "INSERT INTO " + "Students" + " (Date, time, username,selectedgroup) VALUES(@Date, @time, @username,@selectedgroup)";
+                            break;
+                        default:
+                            commandText = "INSERT INTO " + "voicerecord" + " (Date, time, username,selectedgroup) VALUES(@Date, @time, @username,@selectedgroup)";
+                            break;
+                    }
+                    con.Open();
+                    //command.CommandText = @"INSERT INTO voicerecord (Date,time,username) VALUES (@Date,@time,@username)";
+                    command.CommandText = @commandText;
+
+                    command.Parameters.Add(new SqlParameter("@Date", Date));
+                    command.Parameters.Add(new SqlParameter("@time", time));
+                    command.Parameters.Add(new SqlParameter("@username", name));
+                    command.Parameters.Add(new SqlParameter("@selectedgroup", selectedTable));
+
+                    command.ExecuteNonQuery();
+                    Console.WriteLine("{0} success", Date);
+
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception.Message);
+                    throw;
+                }
+                finally
+                {
+                    con.Close();
+                }
+            }
+
+        }
+        private static string selectedTable;
+
+
+        //SQL to get table catalog
+        private static readonly string SelectTableListSql = "SELECT * FROM sys.objects where type='U'";
+
+        //list of table catalog
+        static string[] TableCatalog = new string[10];
+        static List<string> intermediateTableCatalog = new List<string>();
+
+        static void GetTableCatalog()
+        {
+            string constr = @"Data Source=sfws.c7t5mjpl9wt1.ap-northeast-1.rds.amazonaws.com,1433;
+                                Initial Catalog=Workshop;Connect Timeout=20;Persist Security Info=True;User ID=SSD;Password=lcopLCOP";
+            // コネクションを生成します。
+            using (var connection = new SqlConnection(constr))
+                try
+                {
+                    // コマンドオブジェクトを作成します。
+                    using (var command = connection.CreateCommand())
+                    {
+                        // コネクションをオープンします。
+                        connection.Open();
+
+                        // テーブル一覧取得のSQLを実行します。
+                        command.CommandText = SelectTableListSql;
+                        var reader = command.ExecuteReader();
+                        // 取得結果を確認します。
+                        if (reader.HasRows)
+                        {
+                            while (reader.Read())
+                            {
+                                var name = reader["name"] as string;
+                                Console.WriteLine(name);
+                                intermediateTableCatalog.Add(name);
+                            }
+                            TableCatalog = intermediateTableCatalog.ToArray();
+                        }
+                        for (int i = 0; i < 5; i++)
+                        {
+                            Console.WriteLine(TableCatalog[i]);
+                        }
+                    }
+                }
+
+                // 例外が発生した場合
+                catch (Exception e)
+                {
+                    // 例外の内容を表示します。
+                    Console.WriteLine(e.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+        }
+
+        static void timer() //submit data every minitute
+        {
+            DateTime intervaltime = DateTime.Now;
+            int nowMinnite = intervaltime.Minute;
+            int nowSecond = intervaltime.Second;
+            if (nowMinnite % 1 == 0 && nowSecond == 00)   //every minute
+            {
+
+                submitdata(time);
+                System.Diagnostics.Debug.WriteLine("submmit");
+                time = 0;         //reset total speaking time
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (nameInput)
+            {
+                timer();
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            int index = comboBox1.SelectedIndex;
+            selectedTable = comboBox1.Items[index].ToString();
+            System.Diagnostics.Debug.WriteLine("selected table is", selectedTable);
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            //Task.WaitAny(new[] { stopRecognition.Task });
+            //startRecognition(sender,e);
+            
+        }
+    }
+}
